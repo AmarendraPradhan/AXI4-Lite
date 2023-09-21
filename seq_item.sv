6ae203e4504bf83d29106c920e170e2217c41e05
AmarendraPradhan <140310263+AmarendraPradhan@users.noreply.github.com>
2023-09-21 06:22:52 +0530

    verification

--- seq_item.sv
+++ seq_item.sv
@@ -60,7 +60,6 @@ constraint c_wstrb { wstrb  inside
 4'b0111,4'b1000,4'b1001,4'b1010,4'b1011,4'b1100,4'b1101,4'b1111}; }
 
 
-
 // constructor
 function new(string name = "seq_item");
 	   	super.new(name);
